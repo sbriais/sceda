750d37a2f498fb88b40bc1ea1dda41eb72915192
Sébastien Briais <sebastien.briais@gmail.com>
2009-04-27 11:54:23 +0000

    Relabel to front algorithm.

--- src/graph_flow.c
+++ src/graph_flow.c
@@ -72,7 +72,7 @@ SCEDA_HashMap *SCEDA_graph_max_flow(SCEDA_Graph *g, SCEDA_Vertex *s, SCEDA_Verte
     SCEDA_edges_iterator_cleanup(&edges);
   }
 
-  SCEDA_List *todo = SCEDA_list_create(NULL);
+  SCEDA_List *todo = SCEDA_queue_create(NULL);
   {
     SCEDA_VerticesIterator vertices;
     SCEDA_vertices_iterator_init(g, &vertices);
@@ -83,16 +83,16 @@ SCEDA_HashMap *SCEDA_graph_max_flow(SCEDA_Graph *g, SCEDA_Vertex *s, SCEDA_Verte
       }
       boxed(int) ex_v = SCEDA_hashmap_get(excess, v);
       if(boxed_get(ex_v) > 0) {
-	safe_call(SCEDA_list_ins_next(todo, NULL, v));
+	safe_call(SCEDA_queue_enqueue(todo, v));
       }
     }
     SCEDA_vertices_iterator_cleanup(&vertices);
   }
 
   /* Preflow */
-  while(!SCEDA_list_is_empty(todo)) {
+  while(!SCEDA_queue_is_empty(todo)) {
     SCEDA_Vertex *u;
-    safe_call(SCEDA_list_rem_next(todo, NULL, (void **)&u));
+    safe_call(SCEDA_queue_dequeue(todo, (void **)&u));
     if(u == t) {
       continue;
     }
@@ -100,95 +100,101 @@ SCEDA_HashMap *SCEDA_graph_max_flow(SCEDA_Graph *g, SCEDA_Vertex *s, SCEDA_Verte
     boxed(int) ex_u = SCEDA_hashmap_get(excess, u);
     boxed(int) h_u = SCEDA_hashmap_get(height, u);
 
-    // push as much flow as possible
-    {
-      SCEDA_OutEdgesIterator edges;
-      SCEDA_out_edges_iterator_init(u, &edges);
-      while(SCEDA_out_edges_iterator_has_next(&edges)) {
-	SCEDA_Edge *e = SCEDA_out_edges_iterator_next(&edges);
-	boxed(int) fe = SCEDA_hashmap_get(flow, e);
-	// residual_capacity(u,v) = capacity(u,v) - flow(u,v) 
-	int rc = capacity(e, c_ctxt) - boxed_get(fe);
-	if(rc <= 0) {
-	  continue;
+    /* by definition, u is exceeding, 
+       since it was in the queue of exceeding vertices
+       we thus discharge it */
+    do {
+      /* push as much flow as possible */
+      {
+	SCEDA_OutEdgesIterator edges;
+	SCEDA_out_edges_iterator_init(u, &edges);
+	while(SCEDA_out_edges_iterator_has_next(&edges)) {
+	  SCEDA_Edge *e = SCEDA_out_edges_iterator_next(&edges);
+	  boxed(int) fe = SCEDA_hashmap_get(flow, e);
+	  // residual_capacity(u,v) = capacity(u,v) - flow(u,v) 
+	  int rc = capacity(e, c_ctxt) - boxed_get(fe);
+	  if(rc <= 0) {
+	    continue;
+	  }
+	  SCEDA_Vertex *v = SCEDA_edge_target(e);
+	  boxed(int) h_v = SCEDA_hashmap_get(height, v);
+	  if(boxed_get(h_v) < boxed_get(h_u)) {
+	    // h(u) = h(v) + 1 so we can push
+	    boxed(int) ex_v = SCEDA_hashmap_get(excess, v);
+	    if(boxed_get(ex_v) == 0) {
+	      /* v was not exceeding (hence not in the queue)
+		 but will be exceeding now */
+	      safe_call(SCEDA_queue_enqueue(todo, v));
+	    }
+	    /* amount of flow to push */
+	    int push = boxed_get(ex_u);
+	    if(push > rc) {
+	      /* cannot push more than the residual capacity */
+	      push = rc;
+	    }
+	    /* update excess values */
+	    boxed_set(fe, boxed_get(fe) + push);
+	    boxed_set(ex_v, boxed_get(ex_v) + push);
+	    boxed_set(ex_u, boxed_get(ex_u) - push);
+	    if(boxed_get(ex_u) == 0) {
+	      /* nothing to push anymore */
+	      break;
+	    }
+	  }
 	}
-	SCEDA_Vertex *v = SCEDA_edge_target(e);
-	boxed(int) h_v = SCEDA_hashmap_get(height, v);
-	if(boxed_get(h_v) < boxed_get(h_u)) {
-	  // h(u) = h(v) + 1 so we can push
-	  boxed(int) ex_v = SCEDA_hashmap_get(excess, v);
-	  if(boxed_get(ex_v) == 0) {
-	    // v was not exceeding (hence not in the queue)
-	    // but will be exceeding now
-	    safe_call(SCEDA_list_ins_next(todo, SCEDA_list_tail(todo), v));
-	  }
-	  // amount of flow to push 
-	  int push = boxed_get(ex_u);
-	  if(push > rc) {
-	    // cannot push more than the residual capacity
-	    push = rc;
-	  }
-	  // update excess values 
-	  boxed_set(fe, boxed_get(fe) + push);
-	  boxed_set(ex_v, boxed_get(ex_v) + push);
-	  boxed_set(ex_u, boxed_get(ex_u) - push);
-	  if(boxed_get(ex_u) == 0) {
-	    // nothing to push anymore
-	    break;
-	  }
-	}
+	SCEDA_out_edges_iterator_cleanup(&edges);
       }
-      SCEDA_out_edges_iterator_cleanup(&edges);
-    }
 
-    // push back as much flow as possible
-    if(boxed_get(ex_u) > 0) {
-      SCEDA_InEdgesIterator edges;
-      SCEDA_in_edges_iterator_init(u, &edges);
-      while(SCEDA_in_edges_iterator_has_next(&edges)) {
-	SCEDA_Edge *e = SCEDA_in_edges_iterator_next(&edges);
-	boxed(int) fe = SCEDA_hashmap_get(flow, e);
-	if(boxed_get(fe) <= 0) {
-	  continue;
+      /* push back as much flow as possible */
+      if(boxed_get(ex_u) > 0) {
+	/* this is essentially the same algorithm as above
+	   but residual capacity is computed differently */
+	SCEDA_InEdgesIterator edges;
+	SCEDA_in_edges_iterator_init(u, &edges);
+	while(SCEDA_in_edges_iterator_has_next(&edges)) {
+	  SCEDA_Edge *e = SCEDA_in_edges_iterator_next(&edges);
+	  boxed(int) fe = SCEDA_hashmap_get(flow, e);
+	  if(boxed_get(fe) <= 0) {
+	    continue;
+	  }
+	  SCEDA_Vertex *v = SCEDA_edge_source(e);
+	  boxed(int) h_v = SCEDA_hashmap_get(height, v);
+	  if(boxed_get(h_v) < boxed_get(h_u)) {
+	    boxed(int) ex_v = SCEDA_hashmap_get(excess, v);
+	    if(boxed_get(ex_v) == 0) {
+	      safe_call(SCEDA_queue_enqueue(todo, v));
+	    }
+	    int push = boxed_get(ex_u);
+	    if(push > boxed_get(fe)) {
+	      /* cannot push back more than the flow */
+	      push = boxed_get(fe);
+	    }
+	    boxed_set(fe, boxed_get(fe) - push);
+	    boxed_set(ex_v, boxed_get(ex_v) + push);
+	    boxed_set(ex_u, boxed_get(ex_u) - push);
+	    if(boxed_get(ex_u) == 0) {
+	      /* nothing to push anymore */
+	      break;
+	    }
+	  }
 	}
-	SCEDA_Vertex *v = SCEDA_edge_source(e);
-	boxed(int) h_v = SCEDA_hashmap_get(height, v);
-	if(boxed_get(h_v) < boxed_get(h_u)) {
-	  boxed(int) ex_v = SCEDA_hashmap_get(excess, v);
-	  if(boxed_get(ex_v) == 0) {
-	    safe_call(SCEDA_list_ins_next(todo, SCEDA_list_tail(todo), v));
-	  }
-	  int push = boxed_get(ex_u);
-	  if(push > boxed_get(fe)) {
-	    // cannot push back more than the flow
-	    push = boxed_get(fe);
-	  }
-	  boxed_set(fe, boxed_get(fe) - push);
-	  boxed_set(ex_v, boxed_get(ex_v) + push);
-	  boxed_set(ex_u, boxed_get(ex_u) - push);
-	  if(boxed_get(ex_u) == 0) {
-	    // nothing to push anymore
-	    break;
-	  }
-	}
+	SCEDA_in_edges_iterator_cleanup(&edges);
       }
-      SCEDA_in_edges_iterator_cleanup(&edges);
-    }
-
-    // if u is still exceeding, increase its height
-    if(boxed_get(ex_u) > 0) {
-      boxed_set(h_u, boxed_get(h_u) + 1);
-      safe_call(SCEDA_list_ins_next(todo, NULL, u));
-    }
+    
+      /* if u is still exceeding, increase its height (relabel) */
+      if(boxed_get(ex_u) > 0) {
+	boxed_set(h_u, boxed_get(h_u) + 1);
+      }
+    } while(boxed_get(ex_u) > 0);
   }
-
+  
   /* Finished */
-
-  SCEDA_list_delete(todo);
-
+  
+  SCEDA_queue_delete(todo);
+  
   SCEDA_hashmap_delete(height);
   SCEDA_hashmap_delete(excess);
-
+  
   return flow;
 }
 
