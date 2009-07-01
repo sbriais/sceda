15c45f53a53bf01e268f0735f2cd50dda2eb2386
Sébastien Briais <sebastien.briais@gmail.com>
2009-07-01 14:46:45 +0000

    Modified License header.

--- src/pqueue.c
+++ src/pqueue.c
@@ -1,6 +1,6 @@
 /*
    This file is part of SCEDA.
-   (c) 2008-2009 Sebastien Briais
+   Copyright 2008, 2009 Sebastien Briais
    
    SCEDA is free software: you can redistribute it and/or modify it
    under the terms of the GNU Lesser General Public License as
